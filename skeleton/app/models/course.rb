4b14cd0744f0497abb32a3359f4ffcd60384c0ba
tlee <ksltho@gmail.com>
2021-05-20 14:45:39 -0400

    Complete enrollment.rb model

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+
+  has_many :enrollments,
+    class_name: :Enrollment,
+    primary_key: :id,
+    foreign_key: :course_id
+
+
+
 end
