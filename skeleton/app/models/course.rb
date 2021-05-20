f8efada6ffd22ce470ae4fcdaa7d172a23df376e
tlee <ksltho@gmail.com>
2021-05-20 17:08:41 -0400

    Complete Associations Project

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -20,8 +20,28 @@ class Course < ApplicationRecord
     through: :enrollments,
     source:  :user
 
-  has_many :prerequisites,
+  belongs_to :prerequisite,
     primary_key: :id,
     foreign_key: :prereq_id,
     class_name: :Course
+ 
+  belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
 end
+
+#Finally, add an instructor 
+#association to Course. This will
+#point to a User object. Note that
+#Course is now related to User in
+#two ways: instructor and 
+#enrolled_students.
+
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer # foreign key
+#  instructor_id :integer # foreign key # 5
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
